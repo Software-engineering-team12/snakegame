a302a6f415639897d161dea85cdc2efa62440224
Jiwon Jeong <jiwonjeong37@gmail.com>
2022-04-18 22:20:27 +0900

    apple_class 0418 complete

--- apple_class.py
+++ apple_class.py
@@ -4,23 +4,28 @@ from datetime import timedelta
 import random
 import time
 
-win_size = 1600
+WIN_SIZE = (40*20)**2
 row = col = 40
+B_size = WIN_SIZE // row   # block size
 
 # 스크린 객체 저장
-SCREEN = pygame.display.set_mode((row, col))
+SCREEN = pygame.display.set_mode((B_size*row, B_size*col))
 SCREEN.fill((255, 255, 255))
 
 
 class Apple:
-    def __init__(self, position=(100, 100)):
+    def __init__(self, position=(B_size*10, B_size*10)):
         self.pos_x = position[0]
         self.pos_y = position[1]
 
     def draw(self):
         apple = pygame.image.load("fruit.png")
-        apple = pygame.transform.scale(apple, (40, 40))
+        apple = pygame.transform.scale(apple, (25, 25))
         SCREEN.blit(apple, [self.pos_x, self.pos_y])
+
+    def position(self):
+        self.pos_x = random.randint(0, 41)
+        self.pos_y = random.randint(0, 41)
 
 
 apple = Apple()
