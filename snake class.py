22d94dd37a38c4d7830939682fc8370c7af83e91
iimpala <dla4051@naver.com>
2022-04-19 23:38:35 +0900

    modified snake class

--- snake class.py
+++ snake class.py
@@ -1,20 +1,20 @@
 # Snake Class
-
 import pygame
+import numpy as np
 
 KEY = {
-    pygame.K_UP : 'UP',
-    pygame.K_DOWN : 'DOWN',
-    pygame.K_LEFT : 'LEFT',
-    pygame.K_RIGHT : 'RIGHT',
-    pygame.K_ESCAPE : 'ESC',
+    'UP' : pygame.K_UP ,
+    'DOWN' : pygame.K_DOWN,
+    'LEFT' : pygame.K_LEFT,
+    'RIGHT' : pygame.K_RIGHT,
+    'ESC' : pygame.K_ESCAPE,
 }
 
 DIRECTION = {
-    (0, 1) : 'u',
-    (0, -1) : 'd',
-    (1, 0) : 'r',
-    (-1, 0) : 'l'
+    'u' : np.array([0, -1]),
+    'd' : np.array([0, 1]),
+    'r' : np.array([1, 0]),
+    'l' : np.array([-1, 0])
 }
 
 class Snake :
@@ -23,12 +23,12 @@ class Snake :
     
     #Body클래스는 뱀의 몸통 한칸을 뜻하며 위치와 방향을 가지고 있다.
     class Body :
-        def __init__(self, position, direction = 'u') : 
+        def __init__(self, position, direction = DIRECTION['u']) : 
             self.pos = position
             self.direction = direction
 
     #생성자 : 뱀의 머리와 꼬리를 생성한다. 초기상태에서 뱀의 머리와 꼬리는 일치한다.
-    def __init__(self, position) :
+    def __init__(self, position = np.array([20,20])) :
         self.head = Snake.Body(position)
         self.bodys.append(self.head)
         self.tail = self.bodys[-1]
@@ -36,6 +36,7 @@ class Snake :
     #reset 메소드에서는 게임을 다시 시작할 때 뱀을 초기상태로 되돌린다.
     def reset(self, position) :
         self.bodys = []
+        self.turns = {}
         self.head = Snake.Body(position)
         self.bodys.append(self.head)
         self.tail = self.bodys[-1]
@@ -45,31 +46,31 @@ class Snake :
         for event in pygame.event.get() :
             if event.type == pygame.QUIT :
                 pygame.quit()
-            elif event.type == 'ESC' :
+            #elif event.type == KEY['ESC'] :
                 #show_ingame_menu
-                pass
-
+            #    pass
             keys = pygame.key.get_pressed()
+
             for key in keys :
-                if keys['UP'] :
-                    self.turns[self.head.pos[:]] = 'u'
-                elif keys['DOWN'] :
-                    self.turns[self.head.pos[:]] = 'd'
-                elif keys['LEFT'] :
-                    self.turns[self.head.pos[:]] = 'l'
-                elif keys['RIGHT'] :
-                    self.turns[self.head.pos[:]] = 'r'
+                if keys[ KEY['UP'] ] :
+                    self.turns[ tuple(self.head.pos[:]) ] = DIRECTION['u']
+                elif keys[ KEY['DOWN'] ] :
+                    self.turns[ tuple(self.head.pos[:]) ] = DIRECTION['d']
+                elif keys[ KEY['LEFT'] ] :
+                    self.turns[ tuple(self.head.pos[:]) ] = DIRECTION['l']
+                elif keys[ KEY['RIGHT'] ] :
+                    self.turns[ tuple(self.head.pos[:]) ] = DIRECTION['r']
                 
         for i, bloc in enumerate(self.bodys) :
-            p = bloc.pos
+            p = bloc.pos[:]
             d = bloc.direction
-            if p in self.turns :
-                p = p + self.turns[p]
-                d = self.turns[p]
+            if tuple(p) in self.turns :
+                bloc.pos = p + self.turns[tuple(p)]
+                bloc.direction = self.turns[tuple(p)]
                 if i == len(self.bodys) - 1 :
-                    self.turns.pop(p)
+                    self.turns.pop(tuple(p))
             else :
-                p = p + d
+                bloc.pos = p + d
 
 
     #grow 메소드에서는 뱀이 사과를 먹으면 길이가 늘어나는 행동을 취한다. 참고한 코드와 다르게 현재 꼬리자리에 몸통을 한칸 만들고 꼬리를 진행방행과 반대 방향으로 한칸 민다.
@@ -90,5 +91,4 @@ class Snake :
             #    draw_block(window, c.pos, type = 'tail')  #추후 이미지 삽입시 머리, 몸통 꼬리로 나눌수 있다면 사용할 예정입니다.
             else :
                 draw_block(window, bloc.pos, type = 'body')
-
-    
+                
