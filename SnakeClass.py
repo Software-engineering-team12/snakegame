6664275854b1caf58207f3fd02da2a38b7b4aa80
iimpala <dla4051@naver.com>
2022-04-28 00:10:10 +0900

    꼬리 움직임 구현했습니다

--- SnakeClass.py
+++ SnakeClass.py
@@ -2,8 +2,8 @@
 import pygame
 import numpy as np
 
-row = col = 40
-B_size = 20   # block size
+rows = 20
+B_size = 20
 
 DIRECTION = {
     'u' : np.array([0, -1]),
@@ -11,11 +11,6 @@ DIRECTION = {
     'r' : np.array([1, 0]),
     'l' : np.array([-1, 0])
 }
-rows = 20
-def draw_block(screen, position, color=(255,255,0) ):
-    block = pygame.Rect((position[0] * rows, position[1] * rows),
-                        (rows, rows))
-    pygame.draw.rect(screen, color, block)
 
 class Snake :
     bodys = []
@@ -43,9 +38,19 @@ class Snake :
         self.head_right = pygame.image.load("image/head_right.png").convert_alpha()
         self.head_right = pygame.transform.scale(self.head_right, (20, 20))
 
+        self.tail_up = pygame.image.load("image/tail_up.png").convert_alpha()
+        self.tail_up = pygame.transform.scale(self.tail_up, (20, 20))
+        self.tail_down = pygame.image.load("image/tail_down.png").convert_alpha()
+        self.tail_down = pygame.transform.scale(self.tail_down, (20, 20))
+        self.tail_left = pygame.image.load("image/tail_left.png").convert_alpha()
+        self.tail_left = pygame.transform.scale(self.tail_left, (20, 20))
+        self.tail_right = pygame.image.load("image/tail_right.png").convert_alpha()
+        self.tail_right = pygame.transform.scale(self.tail_right, (20, 20))
+
         self.body_image = pygame.image.load("image/body.png").convert_alpha()
         self.body_image = pygame.transform.scale(self.body_image, (20, 20))
-        self.rect_snake = self.body_image.get_rect()  # 이미지 크기와 동일한 사각형 객체 생성
+
+        self.rect_snake = self.body_image.get_rect()                                                # 이미지 크기와 동일한 사각형 객체 생성
 
     #reset 메소드에서는 게임을 다시 시작할 때 뱀을 초기상태로 되돌린다.
     def reset(self, position):
@@ -77,23 +82,18 @@ class Snake :
             else :
                 bloc.pos = p + d
 
-
-    #main함수에서 grow 함수 동작시 문제 발생하여 주석 처리 해놨습니다.
-    #grow 메소드에서는 뱀이 사과를 먹으면 길이가 늘어나는 행동을 취한다. 참고한 코드와 다르게 현재 꼬리자리에 몸통을 한칸 만들고 꼬리를 진행방행과 반대 방향으로 한칸 민다.
+    #grow 메소드에서는 뱀이 사과를 먹으면 몸통의 길이가 늘어난다
     def grow(self):
-        # if self.head == self.tail :
         self.bodys.append(Snake.Body(self.tail.pos - self.tail.direction, self.tail.direction))
         self.tail = self.bodys[-1]
-        # else :
-        #     self.bodys.insert(-2, Snake.Body(self.tail.pos, self.tail.direction))
-        #     self.tail.pos = self.tail.pos - self.tail.direction
 
     #draw 메소드에서는 bodys의 각각의 요소들의 위치를 참고하여 창에 뱀을 그린다.
     def draw(self, screen):
         for i, bloc in enumerate(self.bodys):
-            self.rect_snake.x = B_size * bloc.pos[0]  # 그려야하는 좌표의 위치에 snake 이미지 사이즈만큼의 사각형 객체 생성
+            self.rect_snake.x = B_size * bloc.pos[0]                                                # 그려야하는 좌표의 위치에 snake 이미지 사이즈만큼의 사각형 객체 생성
             self.rect_snake.y = B_size * bloc.pos[1]
-            if i == 0:  # 머리 부분 일 떄
+            
+            if i == 0:                                                                              # 머리 부분 일 떄, 방향에 따라 다른 이미지
                 if np.array_equiv(bloc.direction, DIRECTION['u']) :
                     screen.blit(self.head_up, self.rect_snake)
                 elif np.array_equiv(bloc.direction, DIRECTION['d']) :
@@ -103,7 +103,15 @@ class Snake :
                 elif np.array_equiv(bloc.direction, DIRECTION['r']) :
                     screen.blit(self.head_right, self.rect_snake)
 
-            else:  # 몸통 부분일 때
-                screen.blit(self.body_image, self.rect_snake)
+            elif i == self.bodys.index(self.tail) :                                                 # 꼬리 부분 일 때, 방향에 따라 다른 이미지
+                if np.array_equiv(bloc.direction, DIRECTION['u']) :
+                    screen.blit(self.tail_up, self.rect_snake)
+                elif np.array_equiv(bloc.direction, DIRECTION['d']) :
+                    screen.blit(self.tail_down, self.rect_snake)
+                elif np.array_equiv(bloc.direction, DIRECTION['l']) :
+                    screen.blit(self.tail_left, self.rect_snake)
+                elif np.array_equiv(bloc.direction, DIRECTION['r']) :
+                    screen.blit(self.tail_right, self.rect_snake)
 
-                
+            else:                                                                                   # 몸통 부분일 때
+                screen.blit(self.body_image, self.rect_snake)  
